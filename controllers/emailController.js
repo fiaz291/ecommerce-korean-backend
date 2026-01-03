6ea68e6288c9cec075cc801a47ba4c033298707e
Anish Abid <anish.91@hotmail.com>
2026-01-03 21:57:52 +0500

    ses and forget password api implemented

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,13 +1,5 @@
 const { createResponse } = require('../utils/response');
-const nodemailer = require('nodemailer');
-
-const transporter = nodemailer.createTransport({
-  service: 'Gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+const {verificationEmail} = require('../utils/email');
 
 /**
  * Send Email
@@ -15,31 +7,31 @@ const transporter = nodemailer.createTransport({
  */
 const sendEmail = async (req, res, next) => {
   const { email } = req.body;
+verificationEmail(email);
+  // if (!email) {
+  //   return res.status(400).json(
+  //     createResponse({ error: 'Email is required', status: false })
+  //   );
+  // }
 
-  if (!email) {
-    return res.status(400).json(
-      createResponse({ error: 'Email is required', status: false })
-    );
-  }
+  // try {
+  //   const verificationLink = `${process.env.NEXT_PUBLIC_BASE_URL}/verify-email`;
+  //   await transporter.sendMail({
+  //     from: process.env.EMAIL_USER,
+  //     to: email,
+  //     subject: 'Verify your email',
+  //     html: `<p>Click <a href="${verificationLink}">here</a> to verify your email.</p>`,
+  //   });
 
-  try {
-    const verificationLink = `${process.env.NEXT_PUBLIC_BASE_URL}/verify-email`;
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: email,
-      subject: 'Verify your email',
-      html: `<p>Click <a href="${verificationLink}">here</a> to verify your email.</p>`,
-    });
-
-    return res.status(200).json(
-      createResponse({ message: 'Verification email sent', status: true })
-    );
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json(
-      createResponse({ error: 'Internal server error', status: false })
-    );
-  }
+  //   return res.status(200).json(
+  //     createResponse({ message: 'Verification email sent', status: true })
+  //   );
+  // } catch (error) {
+  //   console.error(error);
+  //   return res.status(500).json(
+  //     createResponse({ error: 'Internal server error', status: false })
+  //   );
+  // }
 };
 
 module.exports = {
