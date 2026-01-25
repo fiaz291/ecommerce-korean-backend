a5df8229f057b5dd9fe8de20d1160bcc3bd5f8dc
fiazsysvoy <fiaz@sysvoy.com>
2026-01-25 18:15:22 +0500

    add all users endpoint admin

--- routes/admin/users.js
+++ routes/admin/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const adminUsersController = require('../../controllers/admin/usersController');
 
 router.get('/', adminUsersController.getAdminUsers);
+router.get('/all-users', adminUsersController.getAllUsers);
 router.get('/get-org-emp', adminUsersController.getOrgEmployees);
 
 module.exports = router;
